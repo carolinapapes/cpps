2699ec1c0d2e2f5012383e8b5a6faf06cdc9d217
Carolina Papes <capapes@car11s2.42barcelona.com>
2026-01-10 18:18:07 +0100

    cpp07 finished

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,79 +6,63 @@
 /*   By: capapes <capapes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:59:30 by capapes           #+#    #+#             */
-/*   Updated: 2026/01/09 15:59:46 by capapes          ###   ########.fr       */
+/*   Updated: 2026/01/10 18:12:53 by capapes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#include <cstddef>   // size_t
-#include <exception> // std::exception
+#include <iostream>
+#include "Array.hpp"
 
-template <typename T>
-class Array {
-public:
-	Array() : _data(0), _size(0) {}
+int main()
+{
+	std::cout << "=== Default constructor ===" << std::endl;
+	Array<int> a;
+	std::cout << "Size of a: " << a.size() << std::endl;
 
-	Array(unsigned int n) : _data(0), _size(n)
+	std::cout << "\n=== Sized constructor ===" << std::endl;
+	Array<int> b(5);
+	std::cout << "Size of b: " << b.size() << std::endl;
+
+	for (size_t i = 0; i < b.size(); ++i)
+		b[i] = static_cast<int>(i * 10);
+
+	for (size_t i = 0; i < b.size(); ++i)
+		std::cout << "b[" << i << "] = " << b[i] << std::endl;
+
+	std::cout << "\n=== Copy constructor ===" << std::endl;
+	Array<int> c(b);
+	for (size_t i = 0; i < c.size(); ++i)
+		std::cout << "c[" << i << "] = " << c[i] << std::endl;
+
+	std::cout << "\n=== Assignment operator ===" << std::endl;
+	Array<int> d;
+	d = b;
+	for (size_t i = 0; i < d.size(); ++i)
+		std::cout << "d[" << i << "] = " << d[i] << std::endl;
+
+	std::cout << "\n=== Const access ===" << std::endl;
+	const Array<int> e(b);
+	for (size_t i = 0; i < e.size(); ++i)
+		std::cout << "e[" << i << "] = " << e[i] << std::endl;
+
+	std::cout << "\n=== Out of bounds test ===" << std::endl;
+	try
 	{
-		if (_size == 0) {
-			_data = 0;
-			return;
-		}
-		_data = new T[_size]; // default-initialized
+		std::cout << b[10] << std::endl; // should throw
+	}
+	catch (std::exception &ex)
+	{
+		std::cout << "Caught exception: " << ex.what() << std::endl;
 	}
 
-	Array(Array const &other) : _data(0), _size(0)
-	{
-		*this = other;
-	}
+	std::cout << "\n=== Different type test (std::string) ===" << std::endl;
+	Array<std::string> s(3);
+	s[0] = "hello";
+	s[1] = "array";
+	s[2] = "template";
 
-	~Array()
-	{
-		delete[] _data;
-	}
+	for (size_t i = 0; i < s.size(); ++i)
+		std::cout << "s[" << i << "] = " << s[i] << std::endl;
 
-	Array &operator=(Array const &other)
-	{
-		if (this == &other)
-			return *this;
-
-		delete[] _data;
-		_data = 0;
-		_size = other._size;
-
-		if (_size == 0)
-			return *this;
-
-		_data = new T[_size];
-		for (size_t i = 0; i < _size; ++i)
-			_data[i] = other._data[i];
-		return *this;
-	}
-
-	class OutOfBoundsException : public std::exception {
-	public:
-		virtual const char *what() const throw() { return "Array index out of bounds"; }
-	};
-
-	T &operator[](size_t idx)
-	{
-		if (idx >= _size)
-			throw OutOfBoundsException();
-		return _data[idx];
-	}
-
-	T const &operator[](size_t idx) const
-	{
-		if (idx >= _size)
-			throw OutOfBoundsException();
-		return _data[idx];
-	}
-
-	size_t size() const { return _size; }
-
-private:
-	T      *_data;
-	size_t  _size;
-};
-
+	return 0;
+}
